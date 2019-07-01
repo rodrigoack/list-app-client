be8513ac861f166f0cbd9be65aeb584c21163eed
Rodrigo Cavichioli <rodrigoack@gmail.com>
2019-07-01 20:27:44 -0300

    Fetch api data using axios at listsContainer component

--- src/components/ListsContainer.js
+++ src/components/ListsContainer.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 class ListsContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lists: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/lists.json')
+         .then(response => {
+          console.log(response);
+          this.setState({
+            lists: response.data
+          });
+         })
+         .catch(error => console.error(error));
+  }
+
   render() {
     return (
       <div className="Lists-container">
